c252855a1777aa520d7b3c4933044c1bb9765913
Henry de Labareyre <henry.delabareyre@octave.biz>
2025-02-23 14:17:08 +0100

    - Refactor 2 endpoints using django rest framework

--- order_app/orders/urls.py
+++ order_app/orders/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.orders, name="orders")
+    path('', views.OrderListView.as_view(), name='order_list'),
+    path('<str:order_id>/', views.OrderDetailView.as_view(), name='order_by_id')
 ]
--- order_app/orders/views.py
+++ order_app/orders/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Order
+from .serializers import OrderSerializer
 
+class OrderListView(APIView):
+    def get(self, request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-def orders(request):
-    return HttpResponse("Hello, world. You're at the polls orders.")
+class OrderDetailView(APIView):
+    def get(self, request, order_id):
+        try:
+            order = Order.objects.get(pk=order_id)
+            serializer = OrderSerializer(order)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
